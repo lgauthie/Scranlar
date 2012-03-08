031674bec737f5a8f67d332e2f24c868108b6a9a
Lee Gauthier <lgauthie@uvic.ca>
2012-03-08 00:23:40 -0800

    Adding some more documentation.

--- src/gnar/gran/seq/SingleTableSequence.scala
+++ src/gnar/gran/seq/SingleTableSequence.scala
@@ -12,13 +12,19 @@ class SingleTableSequence(val table:TableSource, val audio:Audio, grainLengthMs:
   val grainLength = ((audio.AUDIO_FORMAT.getSampleRate()) * (grainLengthMs / 1000.0f)).asInstanceOf[Int]
   val env = new SinEreversenvelope(grainLength)
   val grainList = new ListBuffer[Grain]
-
+  
+  /** Plays all of the grains in the list of grains
+    *
+    **/
   def synthesize = {
     grainList.reverse.foreach { grain =>
       audio.play(audio.floatArrayToByte(grain.synthesize))
     }
   }
 
+  /** Generates a list of grains from the input table
+    * 
+    **/
   def generateGrainList = {
     val end = table.table.length - grainLength
     val step = grainLength
@@ -27,6 +33,13 @@ class SingleTableSequence(val table:TableSource, val audio:Audio, grainLengthMs:
     }
   }
 
+  /** Creates an array of 0's to fill n ms of time
+    * 
+    * @param time the time in ms to fill
+    *
+    * @return an array filled with 0's
+    *
+    **/
   def silence(time:Int) = {
     val end = ((audio.AUDIO_FORMAT.getSampleRate()) * (time / 1000.0f)).asInstanceOf[Int]
     val emptySamples = new ListBuffer[Float]
